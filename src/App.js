0cf84ca5377db1fbeb247ba2c2226decd98ac87f
Juiux <upw.williamscott0920@gmail.com>
2023-03-21 12:35:29 -0400

    add header section
    Co-authored-by: doBestMan <do.best.man@outlook.com>

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 //import components
+import Header from "./components/Header";
+
+//import components
 const App = () => {
   Aos.init();
-  return <h1>Hello World</h1>;
+  return (
+    <div className="flex flex-col max-w-xl mx-auto w-full items-center overflow-hidden">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
