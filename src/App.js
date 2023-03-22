0fa41df6cd38c0656a6ccf896e0c56709ddbe224
Juiux <upw.williamscott0920@gmail.com>
2023-03-22 09:41:11 -0400

    feat: add hero section

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 //import components
+import Hero from "./components/Hero";
 import Header from "./components/Header";
 
-//import components
 const App = () => {
   Aos.init();
   return (
     <div className="flex flex-col max-w-xl mx-auto w-full items-center overflow-hidden">
       <Header />
+      <Hero />
     </div>
   );
 };
