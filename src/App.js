5f42522aacec68c3e1ab25c56ef2b548df3bf154
Juiux <upw.williamscott0920@gmail.com>
2023-03-24 08:37:12 -0400

    feat: added cta section

--- src/App.js
+++ src/App.js
@@ -13,16 +13,20 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Benifit from "./components/Benifit";
 import Trustedmonial from "./components/Trustedmonial";
+import Cta from "./components/Cta";
 
 const App = () => {
   Aos.init();
   return (
+    <h1>Hello World</h1>
     <div className="flex flex-col max-w-xl mx-auto w-full items-center overflow-hidden">
+      
       <Header />
       <Hero />
       <About />
       <Benifit />
       <Trustedmonial />
+      <Cta />
     </div>
   );
 };
